bba27bf1d5f7f831152f7f8f45f4a821524a9e67
nikola.ninov <momzzze@abv.bg>
2024-03-25 20:01:05 +0200

    Update bookController.js

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -42,7 +42,7 @@ router.post('/edit/:id', async (req, res) => {
     }
 });
 
-router.delete('/delete/:id', async (req, res) => {
+router.get('/delete/:id', async (req, res) => {
     const id = req.params.id;
     console.log(id);
     try {
